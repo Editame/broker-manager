9b5d721a92b36180c583598038d2a88c8601b3b8
editapme <edilberto.tapias@ticscaribe.com>
2025-06-08 21:28:21 -0500

    feat: Initial project scaffolding added (main)
    This commit sets up the initial project structure, including:
    
    - Gradle build files
    - Source code directories
    - Test code directories
    - Necessary dependencies
    - Application properties file
    - Git attributes file
    - gitignore file

---
+++ src/main/java/com/editame/brokermanager/BrokerManagerApplication.java
@@ -0,0 +1,13 @@
+package com.editame.brokermanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrokerManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrokerManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/editame/brokermanager/BrokerManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.editame.brokermanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrokerManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
